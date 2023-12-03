b589f9f3341bf5aa59a9ac9165d0ad81358b331e
Hungy <hungngojan25@gmail.com>
2023-12-03 12:58:14 +0700

    mock api for score

--- src/api/routes/score.route.js
+++ src/api/routes/score.route.js
@@ -1,41 +1,177 @@
-const mockData = require('../../data/mockData');
-const { sendRes, sendErr } = require('../helpers/response');
-const scoreRoute = require('express').Router();
-
+const mockData = require("../../data/mockData");
+const { sendRes, sendErr } = require("../helpers/response");
+const scoreRoute = require("express").Router();
 
 // utils API
-scoreRoute.get('/mock/semesters', (req,res) =>{
-  sendRes(res, 200,  mockData.getSemesters());
+
+scoreRoute.get("/mock/semesters", (req, res) => {
+  sendRes(res, 200, mockData.getSemesters());
 });
 
-scoreRoute.get('/mock/scoretypes', (req,res) =>{
-  sendRes(res, 200,  mockData.getScoreTypes());
+scoreRoute.get("/mock/scoretypes", (req, res) => {
+  sendRes(res, 200, mockData.getScoreTypes());
 });
 
-
-scoreRoute.get('/mock/subjects', (req,res) =>{
-  const {teacherId, semesterId }= req.query;
-  if(!teacherId || ! semesterId ){
-    return sendErr(res,{status: 500, message: "Missing required params"})
+scoreRoute.get("/mock/subjects", (req, res) => {
+  const { teacherId, semesterId } = req.query;
+  if (!teacherId || !semesterId) {
+    return sendErr(res, { status: 500, message: "Missing required params" });
   }
-  return sendRes(res, 200,  mockData.getSubjects());
+  return sendRes(res, 200, mockData.getSubjects({ teacherId, semesterId }));
 });
-
 
 // main API:
 
 // - Show current grade structure:
-scoreRoute.post('/mock/grade-structure', (req,res) =>{
-  const {teacherId, subjectId, semesterId} = req.body;
-  sendRes(res, 200,  mockData.getScoreStructure());
+scoreRoute.post("/mock/grade-structure", (req, res) => {
+  const { teacherId, subjectId, semesterId } = req.body;
+  sendRes(
+    res,
+    200,
+    mockData.getScoreStructure({ teacherId, subjectId, semesterId })
+  );
 });
 
-// - Add a grade composition with a name and grade scale
-
-scoreRoute.get('/mock/add-grade-composition', (req,res) =>{
-  const {subjectId,teacherId,semester,scoretype} = req.body;
-  sendRes(res, 200,  mockData.getScoreStructure());
+// - Add a grade composition with a name and grade scale (only choose in grade structure list)
+scoreRoute.post("/mock/add-grade-composition", (req, res) => {
+  const { subjectId, teacherId, semester, scoreTypeId, percentage } = req.body;
+  sendRes(
+    res,
+    200,
+    mockData.addScoreStructure({
+      subjectId,
+      teacherId,
+      semester,
+      scoreTypeId,
+      percentage,
+    })
+  );
 });
 
+// Remove a grade composition
+scoreRoute.post("/mock/remove-grade-composition", (req, res) => {
+  const { subjectId, teacherId, semester, scoreTypeId, percentage } = req.body;
+  sendRes(
+    res,
+    200,
+    mockData.removeScoreStructure({
+      subjectId,
+      teacherId,
+      semester,
+      scoreTypeId,
+      updateScoreStructure,
+    })
+  );
+});
+
+// Update a grade composition (name, grade scale)
+// Mark a grade composition as finalized
+scoreRoute.post("/mock/update-grade-composition", (req, res) => {
+  const { subjectId, teacherId, semester, scoreTypeId, isPublish } = req.body;
+  sendRes(
+    res,
+    200,
+    mockData.updateScoreStructure({
+      subjectId,
+      teacherId,
+      semester,
+      scoreTypeId,
+      isPublish,
+    })
+  );
+});
+
+// Class owner uploads a csv/xlsx file with student list (StudentId, Full name)
+scoreRoute.get("/mock/add-students", (req, res) => {
+  // @todo: handle xls file here and add to database
+  sendRes(res, 200);
+});
+
+// Show Students (pre-upload full student list) x Grades board
+// Show total grade column at grade board
+scoreRoute.get("/mock/scores", (req, res) => {
+  const { subjectId, teacherId, semesterId } = req.query;
+  // @todo: handle xls file here and add to database
+  sendRes(
+    res,
+    200,
+    mockData.getScores({
+      subjectId,
+      teacherId,
+      semesterId,
+    })
+  );
+});
+
+// View list of grade reviews requested by students
+scoreRoute.get("/mock/reviews-requested", (req, res) => {
+  const { subjectId, teacherId, semesterId } = req.query;
+  // @todo: handle xls file here and add to database
+  sendRes(
+    res,
+    200,
+    mockData.getScoresRequested({
+      subjectId,
+      teacherId,
+      semesterId,
+    })
+  );
+});
+
+// View grade review details: Student, grade composition, current grade, student expectation grade, student explanation
+scoreRoute.get("/mock/review-requested-detail", (req, res) => {
+  const { subjectId, teacherId, semesterId, reviewRequestedId } = req.query;
+  // @todo: handle xls file here and add to database
+  sendRes(
+    res,
+    200,
+    mockData.getScoresRequestedDetail({
+      subjectId,
+      teacherId,
+      semesterId,
+      reviewRequestedId,
+    })
+  );
+});
+
+// Comment on a student review
+scoreRoute.get("/mock/comment-review", (req, res) => {
+  const { userId, reviewRequestedId, content } = req.query;
+  // @todo: handle xls file here and add to database
+  sendRes(
+    res,
+    200,
+    mockData.commentReview({
+      userId,
+      reviewRequestedId,
+      content,
+    })
+  );
+});
+
+// Mark the final decision for a student review with an updated grade
+scoreRoute.get("/mock/update-score", (req, res) => {
+  const {
+    studentId,
+    teacherId,
+    semesterId,
+    subjectId,
+    scoreTypeId,
+    scoreValue,
+  } = req.query;
+  // @todo: handle xls file here and add to database
+  sendRes(
+    res,
+    200,
+    mockData.updateScore({
+      studentId,
+      teacherId,
+      semesterId,
+      subjectId,
+      scoreTypeId,
+      scoreValue,
+    })
+  );
+});
 
 module.exports = scoreRoute;
--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,86 +1,237 @@
+const semesters = [
+  {
+    id: 1,
+    semesterName: "Sem 1 2023",
+  },
+  {
+    id: 2,
+    semesterName: "Sem 2 2023",
+  },
+];
+
+const subjects = [
+  {
+    subjectId: 1,
+    subjectName: "Math",
+    teacherId: 1,
+    semesterId: 1,
+  },
+  {
+    subjectId: 2,
+    subjectName: "Science",
+    teacherId: 1,
+    semesterId: 1,
+  },
+];
+
+const scoreTypes = [
+  {
+    scoreTypeId: 1,
+    scoreTypeName: "Mid",
+  },
+  {
+    scoreTypeId: 2,
+    scoreTypeName: "Final",
+  },
+  {
+    scoreTypeId: 3,
+    scoreTypeName: "Test exam",
+  },
+];
+
+const comments = [
+  {
+    userId: 1,
+    comment: "abc",
+    reviewId: 1,
+  },
+  {
+    userId: 2,
+    comment: "cxde",
+    reviewId: 1,
+  },
+];
+
+const scoresRequested = [
+  {
+    id: 1,
+    student: {
+      id: 1,
+      name: "Nguyen Van A",
+    },
+    teacher: {
+      id: 1,
+      name: "Nguyen Van A",
+    },
+    subjectId: 1,
+    semesterId: 1,
+    scoreTypeId: 1,
+    expectedScore: 10,
+    expectedPercent: 0.1,
+    explain: "abc",
+  },
+  {
+    id: 2,
+    student: {
+      id: 1,
+      name: "Nguyen Van A",
+    },
+    teacher: {
+      id: 1,
+      name: "Nguyen Van A",
+    },
+    subjectId: 2,
+    semesterId: 1,
+    scoreTypeId: 1,
+    expectedScore: 10,
+    expectedPercent: 0.1,
+    explain: "abc",
+  },
+];
+
+const mockScoreStructure = [
+  {
+    scoreTypeId: 1,
+    scoreTypeName: "Mid",
+    percentage: 0.5,
+    isPublish: false,
+  },
+  {
+    scoreTypeId: 2,
+    scoreTypeName: "Final",
+    percentage: 0.5,
+    isPublish: false,
+  },
+];
+
+const scores = [
+  {
+    studentId: 1,
+    teacherId: 1,
+    semesterId: 1,
+    subjectId: 1,
+    scoreTypeId: 1,
+    scoreValue: 9.2,
+  },
+  {
+    studentId: 1,
+    teacherId: 1,
+    semesterId: 1,
+    subjectId: 1,
+    scoreTypeId: 2,
+    scoreValue: 8.2,
+  },
+];
+
+const scoresBoard = {
+  subjectId: 1,
+  teacherId: 2,
+  semesterId: 3,
+  studentsScores: [
+    {
+      studentId: 1,
+      studentName: "Nguyen Van A",
+      scores: scores,
+    },
+  ],
+  total: 9.2,
+};
+
 module.exports = MockData = {
-  getScoreStructure: () => {
-    return {
-      subjectId: 123,
-      teacherId: 123,
-      semester: {},
-      scoreTypeId: 1,
-      percentage: 0.2,
-    };
+  getScoreStructure: (params) => {
+    if (!params) {
+      return;
+    }
+    const { teacherId, subjectId, semesterId } = params;
+
+    // get all scorestructure with params in DB
+    const data = mockScoreStructure;
+
+    return data;
   },
-  getMockScores: () => {
-    return [
-      {
-        subjectId: "123",
-        studentId: 123,
-        teacherId: 456,
-        semester: {},
-        scoreTypeId: 1,
-        scoreValue: 1.2,
-      },
-    ];
-  },
-  getStudent: () => {
-    return {
-      id: 123,
-      name: "student 1",
-    };
-  },
-  getTeacher: () => {
-    return {
-      id: 123,
-      name: "student 1",
-    };
-  },
-  getSemesters: () => {
-    return [
-      {
-        id: "sem12023",
-        name: "Sem 1 2023",
-      },
-      {
-        id: "sem22023",
-        name: "Sem 2 2023",
-      },
-    ];
+  addScoreStructure: (params) => {
+    if (!params) {
+      return;
+    }
+    const { subjectId, teacherId, semester, scoreTypeId, percentage } = params;
+
+    let addedItem = scoreTypes[2];
+    console.log(addedItem);
+
+    // @todo: calculate to ensure total percentage is 100%
+
+    addedItem.percentage = percentage || 0.5;
+    return [...mockScoreStructure, addedItem];
   },
 
-  getScoreTypes: () => {
-    return [
-      {
-        id: 1,
-        name: "Diem mieng",
-      },
-      {
-        id: 2,
-        name: "Diem giua ki",
-      },
-      {
-        id: 3,
-        name: "Diem cuoi ki",
-      },
-    ];
+  removeScoreStructure: (params) => {
+    if (!params) {
+      return;
+    }
+    const { subjectId, teacherId, semester, scoreTypeId } = params;
+
+    // @todo: calculate to ensure total percentage is 100%
+
+    return mockScoreStructure.slice(0, 2);
+  },
+
+  updateScoreStructure: (params) => {
+    if (!params) {
+      return;
+    }
+    const { subjectId, teacherId, semester, scoreTypeId, percentage } = params;
+
+    // @todo: calculate to ensure total percentage is 100%
+
+    return mockScoreStructure;
+  },
+
+  getScores: (params) => {
+    if (!params) {
+      return;
+    }
+    const { subjectId, teacherId, semester } = params;
+
+    // @todo: calculate to ensure total percentage is 100%
+    // @todo: handle get score only isPushlish
+
+    return scoresBoard;
+  },
+
+  getSemesters: (params) => {
+    return semesters;
+  },
+
+  getScoreTypes: (params) => {
+    return scoreTypes;
   },
 
   getSubjects: (params) => {
-    if (!params) {
-      return [];
-    }
     const { teacherId, semesterId } = params;
-
-    if (!teacherId || !semesterId) {
-      return [];
-    }
-
-    return [
-      {
-        id: 123,
-        teacherId: 1,
-        semesterId: 1,
-        scores: MockData.getMockScores(),
-        scoreStructure: MockData.getScoreStructure(),
-        isPublished: false,
-      },
-    ];
+    return subjects;
   },
+
+  getScoresRequested: (params) => {
+    const { subjectId, teacherId, semesterId } = params;
+
+    return scoresRequested;
+  },
+
+  getScoresRequestedDetail: (params) => {
+    return {
+      requestDetail: scoresRequested[0],
+      comments: comments,
+    };
+  },
+
+  commentReview: (params) => {
+    return {
+      requestDetail: scoresRequested[0],
+      comments: comments,
+    };
+  },
+
+  updateScore: (params) =>{
+    return scores[0]
+  }
 };
