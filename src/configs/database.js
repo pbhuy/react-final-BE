5d847414e569fed50f5434fb9861ed3da9fde4e3
Bao Huy <pbhuy0909@gmail.com>
2023-12-10 10:01:51 +0700

    fix role registration

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const uri = process.env.DB_URI;
 const uri_local = process.env.DB_URI_LOCAL;
@@ -8,9 +8,9 @@ const db_name = process.env.DB_NAME;
 const connection = async () => {
     try {
         await mongoose.connect(`${uri}/${db_name}`);
-        console.log('Connected to MongoDB');
+        console.log("Connected to MongoDB");
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
+        console.error("Error connecting to MongoDB:", error);
     }
 };
 
