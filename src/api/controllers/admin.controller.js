ce25c20263f8eb365516b4ab5da178a6b75714f9
qhungo0125 <hungngojan25@gmail.com>
2023-12-24 21:23:04 +0700

    add classes route for admin

--- src/api/controllers/admin.controller.js
+++ src/api/controllers/admin.controller.js
@@ -1,11 +1,30 @@
 const ApiError = require('../helpers/error');
 const { sendRes, sendErr } = require('../helpers/response');
 const Account = require('../models/account.model');
+const ClassRoom = require('../models/Classroom/classroom.model');
 
 require('dotenv').config();
 
 module.exports = {
-  getClasses: (req, res) => {},
+  getClasses: async (req, res) => {
+    // pagination
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+
+    if (page < 1 || limit < 1 || skip < 0)
+      return sendErr(res, new ApiError(400, 'Invalid pagination'));
+
+    const classes = await ClassRoom.find(
+      {},
+      {
+        createdAt: 0,
+        updatedAt: 0,
+      }
+    )
+      .skip(skip)
+      .limit(limit);
+    return sendRes(res, 200, { page, limit, classes });
+  },
 
   getAccounts: async (req, res) => {
     // pagination
