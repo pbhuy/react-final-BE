884e4342302136aec6a6f37f7acd958a13f5cea7
Phạm Bảo Huy <86568590+pbhuy@users.noreply.github.com>
2024-01-02 13:31:34 +0700

    Main << Fix/test score (#16)
    * fix class id in score final
    
    * remove files

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -1,35 +1,35 @@
-const passport = require("passport");
-const { Strategy, ExtractJwt } = require("passport-jwt");
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
 
-const passportConfig = require("../../configs/passport");
-const { sendErr } = require("../helpers/response");
-const ApiError = require("../helpers/error");
+const passportConfig = require('../../configs/passport');
+const { sendErr } = require('../helpers/response');
+const ApiError = require('../helpers/error');
 
 const authenticateJWT = (req, res, next) => {
-    passport.authenticate('jwt', { session: false }, (err, account) => {
-        if (err) {
-            console.log(err);
-            return sendErr(res, new ApiError(401, 'Unauthorized'));
-        }
-        if (!account) {
-            return sendErr(
-                res,
-                new ApiError(401, 'Access denied! Missing or invalid token.')
-            );
-        }
-        req._id = account._id;
-        req.role = account.role;
-        return next();
-    })(req, res, next);
+  passport.authenticate('jwt', { session: false }, (err, account) => {
+    if (err) {
+      console.log(err);
+      return sendErr(res, new ApiError(401, 'Unauthorized'));
+    }
+    if (!account) {
+      return sendErr(
+        res,
+        new ApiError(401, 'Access denied! Missing or invalid token.')
+      );
+    }
+    req._id = account._id;
+    req.role = account.role;
+    return next();
+  })(req, res, next);
 };
 
 const authFacebook = (req, res, next) => {
-  passport.authenticate("facebook", { session: false }, (err, data) => {
-    if (err) return sendErr(res, new ApiError(401, "Unauthorized"));
+  passport.authenticate('facebook', { session: false }, (err, data) => {
+    if (err) return sendErr(res, new ApiError(401, 'Unauthorized'));
     if (!data) {
       return sendErr(
         res,
-        new ApiError(401, "Access denied! Missing or invalid token.")
+        new ApiError(401, 'Access denied! Missing or invalid token.')
       );
     }
     req.data = data;
@@ -38,36 +38,29 @@ const authFacebook = (req, res, next) => {
 };
 
 const authorizeStudent = (req, res, next) => {
-  if (req.role === "student") {
+  if (req.role === 'student') {
     return next();
   }
-  return sendErr(res, new ApiError(403, "Access denied!"));
+  return sendErr(res, new ApiError(403, 'Access denied!'));
 };
 
 const authorizeTeacher = (req, res, next) => {
-  if (req.role === "teacher") {
+  if (req.role === 'teacher') {
     return next();
   }
-  return sendErr(res, new ApiError(403, "Access denied!"));
+  return sendErr(res, new ApiError(403, 'Access denied!'));
 };
 
 const authorizeAdmin = (req, res, next) => {
-    if (req.role === 'admin') {
-        return next();
-    }
-    return sendErr(res, new ApiError(403, 'Access denied!'));
-};
-
-const authorizeAdmin = (req, res, next) => {
-    if (req.role === 'admin') {
-        return next();
-    }
-    return sendErr(res, new ApiError(403, 'Access denied!'));
+  if (req.role === 'admin') {
+    return next();
+  }
+  return sendErr(res, new ApiError(403, 'Access denied!'));
 };
 
 module.exports = {
-    authenticateJWT,
-    authorizeStudent,
-    authorizeTeacher,
-    authFacebook,
+  authenticateJWT,
+  authorizeStudent,
+  authorizeTeacher,
+  authFacebook,
 };
