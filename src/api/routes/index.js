2dbf4aedfd97ec684e567485f479d68081e3ada7
Hung Ngo <hungngojan25@gmail.com>
2024-01-09 13:09:31 +0700

    add notif route

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,15 +1,17 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const accountRoute = require("./account.route");
-const adminRoute = require("./admin.route");
-const classRoute = require("./class.route");
-const classesRoute = require("./classes.route");
-const scoreRoute = require("./score.route");
+const accountRoute = require('./account.route');
+const adminRoute = require('./admin.route');
+const classRoute = require('./class.route');
+const classesRoute = require('./classes.route');
+const notifRoute = require('./notification.route');
+const scoreRoute = require('./score.route');
 
-router.use("/class", classRoute);
-router.use("/classes", classesRoute);
-router.use("/accounts", accountRoute);
-router.use("/score", scoreRoute);
-router.use("/admin", adminRoute);
+router.use('/class', classRoute);
+router.use('/classes', classesRoute);
+router.use('/accounts', accountRoute);
+router.use('/score', scoreRoute);
+router.use('/admin', adminRoute);
+router.use('/notification', notifRoute);
 
 module.exports = router;
